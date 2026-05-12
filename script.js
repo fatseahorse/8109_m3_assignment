d3cd2030fa3d1d190d91ab414b93d6598855172f
Fat Seahorse <28702633+fatseahorse@users.noreply.github.com>
2026-05-12 12:33:51 +0000

    update price list function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let our_quote_items = []; // our price list
 let c_quote_items = []; // customer quote items
-
+let total_price = 0;
 
 document.addEventListener("DOMContentLoaded", async function() {
 
@@ -16,56 +16,57 @@ document.addEventListener("DOMContentLoaded", async function() {
 function main() 
 {  
 
-    addQuoteItem(c_quote_items, "seo03");
-    addQuoteItem(c_quote_items, "smm01");
-    addQuoteItem(c_quote_items, "smm02");
-
-console.log(c_quote_items);
+    console.log(c_quote_items);
+    
+    total_price = 0;
     renderQuote(c_quote_items);
-
 
 }
 
 
-
+// load initial data (price list)
 async function loadData() 
 {
+    // get price list
     const response = await axios.get("/data.json");
-
     our_quote_items = response.data.quote_items;
 console.log(our_quote_items);
 
+    // render price list
+    /*
     let oneItemHTML = "";
 
     for (let qi of our_quote_items) 
     {
         oneItemHTML += `<li>
         
-        <button id="btn-${qi.quote_item_id}" class="btn btn-primary mb-3" data-qid="${qi.quote_item_id}">Add Item</button> | 
+        <button id="btn-${qi.quote_item_id}" class="btn btn-primary mb-3" data-qid="${qi.quote_item_id}" onClick="addToQuoteList(this.dataset.qid);">Add Item</button> | 
         ${qi.quote_item_category} | 
         ${qi.quote_item_id} | 
         ${qi.quote_item_name} | 
         ${qi.quote_item_unit_price}
         </li>`;
-
-
-
-
         
     }
+        document.querySelector("#price-list").innerHTML = "<ul>" + oneItemHTML + "</ul>";
 
-    document.querySelector("#price-list").innerHTML = "<ul>" + oneItemHTML + "</ul>";
+*/
 
+renderPriceList(our_quote_items);
+
+    
 }
 
-function renderQuote(c_quote_items) {
-    const quoteItemList = document.querySelector("#customer-list-table-body");
+function renderPriceList(in_quote_items) 
+{
+    const quoteItemList = document.querySelector("#our-list-table-body");
     quoteItemList.innerHTML = '';
     
-    for (let item of c_quote_items) 
+    for (let item of in_quote_items) 
     {
       const elTR = document.createElement('tr');
       elTR.innerHTML = `
+        <td><button id="btn-add-${item.quote_item_id}" class="btn btn-primary mb-3" data-qid="${item.quote_item_id}" onClick="addToQuoteList(this.dataset.qid);">Add Item</button></td>
         <td>${item.quote_item_category}</td>
         <td>${item.quote_item_id}</td>
         <td>${item.quote_item_name}</td>
@@ -73,6 +74,44 @@ function renderQuote(c_quote_items) {
       `;
       quoteItemList.appendChild(elTR);
     }
-  }
+}
+
+function renderQuote(in_quote_items) 
+{
+    const quoteItemList = document.querySelector("#customer-list-table-body");
+    quoteItemList.innerHTML = '';
+    
+    for (let item of in_quote_items) 
+    {
+      const elTR = document.createElement('tr');
+      elTR.innerHTML = `
+        <td><button id="btn-del-${item.quote_item_uuid}" class="btn btn-primary mb-3" data-qid="${item.quote_item_id}" data-uuid="${item.quote_item_uuid}" onClick="deleteFromQuoteList(this.dataset.uuid);">Remove Item</button></td>
+        <td>${item.quote_item_category}</td>
+        <td>${item.quote_item_id}</td>
+        <td>${item.quote_item_name}</td>
+        <td>$${item.quote_item_unit_price.toFixed(2)}</td>
+      `;
+      quoteItemList.appendChild(elTR);
+    }
+}
 
 
+function addToQuoteList(in_QID)
+{
+
+    console.log(in_QID);
+    addQuoteItem(c_quote_items, in_QID);
+    renderQuote(c_quote_items);
+
+}
+
+
+function deleteFromQuoteList(in_UUID)
+{
+console.log(in_UUID);
+
+    deleteQuoteItem(c_quote_items, in_UUID);
+    renderQuote(c_quote_items);
+
+
+}
